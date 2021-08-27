70b2b0b3a46be8c9b80661031531601058d68d3d
chinhvu97 <chinhvm54.ymc@gmail.com>
2021-08-27 16:45:09 +0700

    upload dynamic way

--- menu/app.js
+++ menu/app.js
@@ -5,7 +5,7 @@ const menu = [
     category: "breakfast",
     price: 15.99,
     img: "./images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: `Perfect, just perfect. Doc, look, all we need is a little plutonium. `,
   },
   {
     id: 2,
@@ -13,7 +13,8 @@ const menu = [
     category: "lunch",
     price: 13.99,
     img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    desc: `Watch this. Not me, the car, the car. My calculations are correct, 
+    when this baby hits eighty-eight miles per hour, your gonna see some serious ***.  `,
   },
   {
     id: 3,
@@ -21,7 +22,7 @@ const menu = [
     category: "shakes",
     price: 6.99,
     img: "./images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    desc: `Okay, real mature guys. Okay, Biff, will you pick up my books? Stop it.`,
   },
   {
     id: 4,
@@ -29,7 +30,7 @@ const menu = [
     category: "breakfast",
     price: 20.99,
     img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    desc: `Uh, I think so. What did you sleep in your clothes again last night. `,
   },
   {
     id: 5,
@@ -37,7 +38,8 @@ const menu = [
     category: "lunch",
     price: 22.99,
     img: "./images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    desc: `Hello. My god, they found me. I don't know how but they found me. Run for it, Marty. 
+    My god, they found me. I don't know how but they found me. Run for it, Marty. `,
   },
   {
     id: 6,
@@ -45,7 +47,7 @@ const menu = [
     category: "shakes",
     price: 18.99,
     img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    desc: `Uh, look me up when you get there. You broke it. Wow, look at him go.`,
   },
   {
     id: 7,
@@ -53,7 +55,7 @@ const menu = [
     category: "breakfast",
     price: 8.99,
     img: "./images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    desc: `He's a peeping tom. Dad. Look at the time, you've got less than 4 minutes, please hurry. `,
   },
   {
     id: 8,
@@ -61,7 +63,7 @@ const menu = [
     category: "lunch",
     price: 12.99,
     img: "./images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    desc: `Look, you gotta listen to me. But I can't go to the dance, I'll miss my favorite television program, Science Fiction Theater.  `,
   },
   {
     id: 9,
@@ -69,13 +71,21 @@ const menu = [
     category: "shakes",
     price: 16.99,
     img: "./images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    desc: `I'm sure that in 1985, plutonium is available at every corner drug store, but in 1955, it's a little hard to come by.
+     Marty, I'm sorry, but I'm afraid you're stuck here.`,
+  },
+  {
+    id: 10,
+    title: "beef steak",
+    category: "dinner",
+    price: 37.99,
+    img: "./images/item-10.jpeg",
+    desc: `Why do you keep following me around? Marty, don't go this way. Strickland's looking for you. If you're caught it'll be four tardies in a row.`,
   },
 ];
 
 const sectionCenter = document.querySelector('.section-center');
-const filter_btns = document.querySelectorAll('.filter-btn');
-
+const btn_container_div = document.querySelector('.btn-container');
 
 
 function displayMenuItems(menuItems) {
@@ -94,25 +104,54 @@ function displayMenuItems(menuItems) {
   displayMenu = displayMenu.join('');
   sectionCenter.innerHTML = displayMenu;
 }
+
+
+function filterEvent(filter_btns) {
+  filter_btns.forEach( (btn) => {
+    btn.addEventListener('click', (e) => {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === 'all') {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    })
+  })
+}
+
+function displayButtons() {
+  //display when load page
+  const categories = menu.reduce((values, item) => {
+    if (!values.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values;
+  }, ['all']);
+
+  const categoryBtns = categories.map((category) => {
+    return `<button class="filter-btn" type="button" data-id=${category}>
+          ${category}
+        </button>`;
+  }).join('');
+  btn_container_div.innerHTML = categoryBtns;
+  const filter_btns = btn_container_div.querySelectorAll('.filter-btn');
+
+  //when user click buttons
+  filterEvent(filter_btns);
+}
+
+
 //load item
 window.addEventListener('DOMContentLoaded', () => {
   displayMenuItems(menu);
+  displayButtons();
 })
 
-//filter
 
-filter_btns.forEach( (btn) => {
-  btn.addEventListener('click', (e) => {
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function (menuItem) {
-      if (menuItem.category === category) {
-        return menuItem;
-      }
-    });
-    if (category === 'all') {
-      displayMenuItems(menu);
-    } else {
-      displayMenuItems(menuCategory);
-    }
-  })
-})
+
+
